4bb863da7304a4b5e3deaa926dbe8f12064b7965
Chris Burton <chburton182@gmail.com>
2021-12-28 21:54:28 -0500

    Added full view section for project 2

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 "use strict";
+/* What this script does:
+1.) Sets up functionality for expanding/collapsing .project-fv sections through their respective buttons.
+2.) Sets up IntersectionsObserver to reveal Contact Form on scroll.
+ */
 
+/* ******************************************************************* */
+/* QUERY SELECTORS */
 const learnMore = document.querySelectorAll(".project__link");
 
 const projectImg = document.querySelectorAll(".project__img");
-/* projectImg contains all images within a project that serve as links */
+/* projectImg contains all images within a .project that serve as links */
 
 const expandBtns = [...learnMore].concat([...projectImg]);
 /* expandBtns contains all links that direct to a full-view project */
@@ -13,56 +19,64 @@ const collapseBtns = document.querySelectorAll(".collapse");
 
 const formContainer = document.querySelector(".contact__form-container");
 
-// Find the Target ID
-const findTarget = function (e) {
-  console.log("E.TARGET:");
-  console.log(e.target);
-  const temp = e.target.dataset.target;
+/* ********************************************************************** */
+/* FUNCTIONALITY FOR REVEALING .PROJECT-FV SECTIONS */
 
-  const target = document.getElementById(temp);
-  console.log(target);
-  return target;
-  /* The returned target is the div that the link's href points to */
+// Find the Full-View ID
+const findFullView = function (e) {
+  // Links in .project and .project-fv contain a data-target attribute, pointing to the ID of the .project-fv section that should be expanded/collapsed when button is clicked.  Store the id in fullViewID.
+  const fullViewID = e.target.dataset.target;
+  console.log(fullViewID);
+  // Find the full-view section via ID and return.
+  const fullView = document.getElementById(fullViewID);
+  return fullView;
 };
 
 // Expand View
 const expandView = function (e) {
-  const target = findTarget(e);
-  target.classList.add("active");
+  // asks findFullView() to determine the .project-fv to expand.
+  const fullView = findFullView(e);
+  // adds .active class to fullView, expanding and revealing the .project-fv.
+  fullView.classList.add("active");
 };
 
 // Collapse View
 const collapseView = function (e) {
-  const target = findTarget(e);
-  target.classList.remove("active");
+  //asks findFullView() to determine the .project-fv to collapse.
+  const fullView = findFullView(e);
+
+  // removes .active class from fullView, collapsing and hiding the .project-fv.
+  fullView.classList.remove("active");
 };
 
-// Scroll to Target
+// Setting up Event Listeners for Expand and Collapse Btns.  expandBtns will now reveal their respective .project-fv and collapseBtns will do the opposite.
 expandBtns.forEach((btn) => btn.addEventListener("click", expandView));
 collapseBtns.forEach((btn) => btn.addEventListener("click", collapseView));
 
-/* ******************************** */
-/* REVEAL FORM CONTAINER ON SCROLL */
+/* ************************************************************************* */
+/*FUNCTIONALITY FOR REVEALING FORM CONTAINER ON SCROLL */
 
 /* Add .hidden class to __form-container */
 formContainer.classList.add("hidden");
 
-//Once the __form-container passes 20% through the viewport, reveal the container by removing the .hidden class.  Then unobserve the container.
+//Once the __form-container passes 20% through the viewport, reveal the container by removing the .hidden class.  Then unobserve the container.  Transition animations are handled through CSS.
 
 const obsCallback = function (entries, observer) {
   const [entry] = entries;
   if (!entry.isIntersecting) return;
   entry.target.classList.remove("hidden");
-  console.log(entry);
-  // observer.unobserve(entry.target);
+  //Once target passes the root threshold, the .hidden class will be removed.
+  observer.unobserve(entry.target);
+  //Removing observer for performance.
 };
 const obsOptions = {
   root: null,
+  // null = viewport
   threshold: 0.2,
+  // threshold = 20%
 };
-const formObserver = new IntersectionObserver(obsCallback, obsOptions);
-console.log(formObserver);
 
+const formObserver = new IntersectionObserver(obsCallback, obsOptions);
 formObserver.observe(formContainer);
 
 /* ********************************************* */
